4971d0c6040aa37318f04d53a3126a332f44d874
faseehbhatti <55502131+faseehbhatti@users.noreply.github.com>
2022-05-23 13:43:34 -0400

    Add files via upload

---
+++ ImageViewApp_Bhatti/app/src/main/java/com/bhattifaseeh/imageviewapp_bhatti/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bhattifaseeh.imageviewapp_bhatti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+// “On my honor, I have neither received nor given any unauthorized assistance on Lab 1.” - FB
